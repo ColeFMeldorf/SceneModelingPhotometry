bcdf9e6713d20ff9314e842446afa4962208b0c8
Jason Lee <astjason@sas.upenn.edu>
2024-06-06 08:25:36 -0700

    Adding multismp_for_SN.py and multismp_for_SN_z_band.py, can give source and background priors + fixed indexing bug when detection not always True + chi2min for chosen images + IZ color for z_band

---
+++ multismp_for_SN_z_band.py
@@ -0,0 +1,473 @@
+from smp import *
+
+
+class MultiDetection(Detection):
+	"""
+	Main class for SMP. Requires RA and Dec for the detection, an exposure and CCD numbers for bookkeeping and
+	zero-point retrieval, a band (for finding extra exposures) and an optional color (for astrometry) and name for the
+	detection
+	"""
+
+	def __init__(self, ra, dec, expnum, ccdnum, band, color = 0.61, color_iz = 0.2, name="", color_bg = 0.61, color_bg_iz = 0.2):
+		"""
+		Constructor class
+		"""
+		self.ra = ra
+		self.dec = dec
+		self.expnum = expnum
+		self.ccdnum = ccdnum
+		self.band = band
+		self.color = color
+		self.color_iz = color_iz
+		self.name = name
+		self.color_bg = color_bg
+		self.color_bg_iz = color_bg_iz
+	def produceExposureList(self, exposures, detected, ra_center = None, dec_center = None):
+		"""
+		The key difference between this and `findAllExposures` is that here I will assume that the list of exposures
+		already exists somewhere
+		"""
+		ccdlist = tb.Table()
+		ccdlist['EXPNUM'] = exposures['EXPNUM']
+		ccdlist['CCDNUM'] = exposures['CCDNUM']
+		ccdlist['BAND'] = exposures['BAND']
+		ccdlist.sort("EXPNUM")
+
+		ccdlist["DETECTED"] = False
+		ccdlist["DETECTED"][np.isin(ccdlist["EXPNUM"], detected)] = True
+
+		self.exposures = tb.unique(ccdlist)
+		if ra_center is None:
+			self.ra_center = {}
+			self.dec_center = {}
+			for i in ccdlist['EXPNUM']:
+				self.ra_center[i] = self.ra 
+				self.dec_center[i] = self.dec 
+		else:
+			self.ra_center = ra_center
+			self.dec_center = dec_center
+
+
+	def findAllExposures(self, survey, detected, return_list=False, reduce_band=True):
+		"""
+		Requires a list of DECamExposures or DESExposures from `DESTNOSIM`,
+		returns all exposures that touch the point.
+		If return_list == True, returns this as a list, otherwise saves this
+		inside det.exposures
+		If reduce_band == True, drops all exposures from different bands (that is,
+		keeps only the same band as the detection)
+		"""
+		from pixmappy import Gnomonic
+
+		x, y = Gnomonic(self.ra, self.dec).toXY(
+			np.array(survey.ra), np.array(survey.dec)
+		)
+		# note that, at RA0, DEC0, x,y = 0
+		dist = np.sqrt(x**2 + y**2)
+
+		close = np.where(dist < 1.5)
+		ra_arr = np.array([self.ra])
+		dec_arr = np.array([self.dec])
+
+		ccdlist = tb.Table(
+			names=("EXPNUM", "CCDNUM", "BAND"), dtype=("i8", "i4", "str")
+		)
+		ccdlist.add_row([self.expnum, self.ccdnum, self.band])
+
+		for i, j in zip(survey.expnum[close], survey.band[close]):
+			ccd = survey[i].checkInCCDFast(ra_arr, dec_arr, ccdsize=0.149931)[1]
+			if ccd != None:
+				ccdlist.add_row([i, ccd, j])
+		ccdlist.sort("EXPNUM")
+
+		ccdlist["DETECTED"] = False
+		ccdlist["DETECTED"][np.isin(ccdlist["EXPNUM"], detected)] = True
+
+		self.exposures = tb.unique(ccdlist)
+
+		if reduce_band:
+			self.exposures = self.exposures[self.exposures["BAND"] == self.band]
+
+		self.exposures.sort("DETECTED")
+
+		if return_list:
+			return self.exposures
+
+	def constructPSFs(
+		self,
+		ra_grid,
+		dec_grid,
+		pmc=DESMaps(),
+		size=30,
+		shift_x=0,
+		shift_y=0,
+		path="",
+	
+	):
+		"""
+		Constructs the PIFF PSFs for the detections, requires an array of RA and Decs (ra_grid, dec_grid), a pixmappy instance (pmc),
+		a stamp size, a potential offset in pixels for the center (shift_x,y), a path for the
+		PIFF files.
+		sparse turns on the sparse matrix solution (uses less memory and can be faster, but less stable)
+		"""
+		psf_matrix = []
+		self.psf = {}
+		for i in self.exposures:
+			try:
+				x_cen, y_cen, wcs = self.findPixelCoords(
+					i["EXPNUM"],
+					int(i["CCDNUM"]),
+					pmc=pmc,
+					return_wcs=True,
+					color=self.color_bg,
+				)
+				self.psf[i['EXPNUM']] = piff.PSF.read(
+					f"{path}/{i['EXPNUM']}/{i['EXPNUM']}_{i['CCDNUM']}_piff.fits"
+				)
+			except (OSError, ValueError):
+				print(f"Missing {i['EXPNUM']} {i['CCDNUM']} psf")
+				psf_matrix.append(sp.csr_matrix(np.zeros((size * size, len(ra_grid)))))
+				continue
+			psf_matrix.append(
+				sp.csr_matrix(
+					construct_psf_background_z(ra_grid, dec_grid, wcs, self.psf[i['EXPNUM']], x_cen, y_cen, size, flatten=True, color=self.color_bg, iz_color = self.color_bg_iz)))
+		print("PSF matrix")
+		self.psf_matrix = sp.vstack(psf_matrix)
+		del psf_matrix
+		self.source_matrix = []
+		self.x_cen = {}
+		self.y_cen = {}
+		self.x_source = {}
+		self.y_source = {}
+		for i in self.exposures[self.exposures["DETECTED"]]:
+			self.x_cen[i['EXPNUM']], self.y_cen[i['EXPNUM']] = self.findPixelCoords(expnum = i['EXPNUM'],ccdnum = i['CCDNUM'], pmc=pmc, color=self.color[i['EXPNUM']], ra=self.ra_center[i['EXPNUM']], dec=self.dec_center[i['EXPNUM']])
+			self.x_source[i['EXPNUM']], self.y_source[i['EXPNUM']] = self.findPixelCoords(expnum = i['EXPNUM'],ccdnum = i['CCDNUM'],pmc=pmc, color=self.color[i['EXPNUM']])
+			try:
+				self.source_matrix.append(
+					construct_psf_source_z(
+						self.x_source[i['EXPNUM']] + shift_x[i["EXPNUM"]],
+						self.y_source[i['EXPNUM']] + shift_y[i["EXPNUM"]],
+						psf=self.psf[i["EXPNUM"]],
+						stampsize=size,
+						x_center=self.x_cen[i['EXPNUM']],
+						y_center=self.y_cen[i['EXPNUM']],
+						iz_color = self.color_iz[i['EXPNUM']]
+					)
+				)
+			except (OSError):
+					print(i['EXPNUM'])
+					self.source_matrix.append(np.zeros((size * size)))
+
+
+	def constructDesignMatrix(self, size, background=False):
+		"""
+		Constructs the design matrix for the solution.
+		size is the stamp size, sparse turns on the sparse solution
+		background defines whether the background is being fit together with the image or not
+		"""
+		if not background:
+			ones = np.ones((size * size, 1))
+		else:
+			ones = np.zeros((size * size, 1))
+
+		print("Background")
+		self.background = sp.block_diag(len(self.exposures) * [ones])
+	
+		self.ntot = len(self.exposures)
+		self.ndet = len(self.exposures[self.exposures["DETECTED"]])
+		self.psf_zeros = np.zeros((self.psf_matrix.shape[0], self.ndet))
+		for i in range(self.ndet):
+			self.psf_zeros[
+				i * size * size : (i + 1) * size * size, i
+			] = self.source_matrix[i]
+		#for i in range(self.ndet):
+		#	self.psf_zeros[
+		#		(self.ntot - self.ndet + i) * size * size : (self.ntot - self.ndet + i + 1) * size * size, (self.ntot - self.ndet) + i
+		#	] = self.source_matrix[i]
+
+		print("Design")
+		self.design = sp.hstack(
+			[self.psf_matrix, self.background, self.psf_zeros], dtype="float64"
+		)
+#Updating updateDesignMatrix as well (getting rid of self.ntot - self.ndet + index), 240516: 
+	def updateDesignMatrix(self, size, index, source_prior_mean = 0, source_prior_weight = 1, source_prior_inds = np.array([25, 50]), bg_prior_mean = 0, bg_prior_weight = 1):
+		self.psf_zeros[index * size * size : (index + 1) * size * size, index] = self.source_matrix[index]
+#		self.psf_zeros[(self.ntot - self.ndet + index) * size * size : (self.ntot - self.ndet + index + 1) * size * size, index] = self.source_matrix[index]
+		self.design = sp.hstack([self.psf_matrix, self.background, self.psf_zeros], dtype="float64")
+		prior_matrix = np.ones(self.design.shape[1])
+		prior_matrix[self.psf_matrix.shape[1]:] = 0
+		source_ind_st = self.psf_matrix.shape[1] + self.ndet + source_prior_inds[0]
+		source_ind_end = source_ind_st + source_prior_inds[1]
+		prior_matrix[source_ind_st:source_ind_end] = 1
+		self.design = sp.vstack([self.design, np.diag(prior_matrix)], dtype="float64")
+#Need to set source prior mean, weights, indices as well as background prior mean, weight (designed for stars), set mean and weight to zero for SN
+	def solvePhotometry(self, res=True, err=True, source_prior_mean = 0, source_prior_weight = 1, source_prior_inds = np.array([25, 50]), bg_prior_mean = 0, bg_prior_weight = 1):
+		"""
+		Solves the system for the flux as well as background sources
+		Solution is saved in det.X, the flux is the -1 entry in this array
+		- res: defines if the residuals should be computed
+		- err: defines if the errors should be computed (requires an expensive matrix inversion)
+		- sparse: turns on sparse routines. Less stable, possibly incompatible with `err`
+		"""
+		self.source_prior_mean = source_prior_mean
+		self.source_prior_weight = source_prior_weight
+		self.bg_prior_mean = bg_prior_mean
+		self.bg_prior_weight = bg_prior_weight
+		prior_matrix = np.ones(self.design.shape[1])
+		prior_matrix[self.psf_matrix.shape[1]:] = 0
+		source_ind_st = self.psf_matrix.shape[1] + self.ndet + source_prior_inds[0]
+		source_ind_end = source_ind_st + source_prior_inds[1]
+		prior_matrix[source_ind_st:source_ind_end] = 1
+		design_with_prior = sp.vstack([self.design, np.diag(prior_matrix)])
+		inv_weight_prior_array = np.ones(self.design.shape[1]) * bg_prior_weight
+		inv_weight_prior_array[self.psf_matrix.shape[1]:] = 0
+		inv_weight_prior_array[source_ind_st:source_ind_end] = source_prior_weight
+		inv_weight_prior = np.concatenate([self.invwgt, inv_weight_prior_array])
+		self.diag = sp.diags(np.sqrt(inv_weight_prior))
+		print("Product")
+		#prod = self.diag.dot(self.design)
+		prod = self.diag.dot(design_with_prior)
+		#self.target = np.concatenate(prior_mean_vals, self.image * np.sqrt(self.invwgt)) # Concatenate zeros for the prior elements
+		target_array = np.ones(self.design.shape[1]) * bg_prior_mean
+		target_array[self.psf_matrix.shape[1]:] = 0
+		target_array[source_ind_st:source_ind_end] = source_prior_mean
+		self.target = np.concatenate([self.image, target_array]) * np.sqrt(inv_weight_prior)
+		print("Solving")
+		self.lsqr = sp.linalg.lsqr(prod, self.target)
+		self.X = self.lsqr[0]
+		print("Solved")
+		self.flux = self.X[-self.ndet:]
+
+		self.mag = -2.5 * np.log10(self.flux) + 30
+
+		if res:
+			self.pred = self.design @ self.X
+			self.res = self.pred - self.image
+
+		if err:
+			inv_cov = self.design.T @ np.diag(self.invwgt) @ self.design
+			try:
+				self.cov = np.linalg.inv(inv_cov)
+			except LinAlgError:
+				self.cov = np.linalg.pinv(inv_cov)
+
+			self.sigma_flux = np.sqrt(self.cov[-self.ndet:, -self.ndet:])
+			self.sigma_mag = (
+				2.5 * np.sqrt(self.cov[-1, -1] / (self.flux**2)) / np.log(10)
+			)
+
+	def runPhotometry(
+		self,
+		se_path,
+		piff_path,
+		zp,
+		survey,
+		detected,
+		pmc=DESMaps(),
+		n_grid=20,
+		size=30,
+		offset_x=0,
+		offset_y=0,
+		err=True,
+		res=True,
+		background=False,
+	):
+		"""
+		Convenience function that performs all operations required by the photometry
+		- se_path: path for the SE postage stamps
+		- piff_path: path for the PIFF files
+		- zp: zeropoint dictionary
+		- survey: `DESTNOSIM` list of exposures
+		- pmc: pixmappy instance for astrometry
+		- n_grid: grid size for point sources in the background (adds n_grid x n_grid sources)
+		- size: stamp size
+		- offset_x,y: offset in the x and y pixel coordinates
+		- sparse: sparse routines
+		- err: turns on error estimation
+		- res: computes residuals
+		- background: background estimation
+		"""
+		self.findAllExposures(survey, detected)
+
+		ra_grid, dec_grid = local_grid(
+			self.ra,
+			self.dec,
+			0.35 / 3600,
+			n_grid,
+		)
+
+		self.constructImages(zp, se_path, size=size, background=background)
+
+		self.constructPSFs(
+			ra_grid, dec_grid, pmc, size, offset_x, offset_y, piff_path)
+
+		self.constructDesignMatrix(size, background=background)
+		self.solvePhotometry(err=err, res=res)
+
+	def photometryShotNoise(self, stampsize, gain_dict):
+		"""
+		Adds in shot noise estimates from a previous fit
+		"""
+
+		if self.flux > 0:
+			## fight gain
+			gain_cut = gain(
+				self.expnum, self.ccdnum, self.x, self.y, stampsize, gain_dict
+			)
+			gain_cut /= self.zp
+
+			sigma_photon = self.pred[-stampsize * stampsize :] / gain_cut.flatten()
+			sigma_photon[sigma_photon < 0] = 0
+			sigma_photon[np.isnan(sigma_photon)] = 0
+			sigma_photon[np.isinf(sigma_photon)] = 0
+			## update weights
+			self.wgt_shotnoise = np.copy(self.wgt)
+
+			self.wgt_shotnoise[-stampsize * stampsize :] += sigma_photon
+
+			self.invwgt_shotnoise = 1 / self.wgt_shotnoise
+			self.invwgt_shotnoise[self.wgt_shotnoise == 0] = 0
+
+			self.invwgt_shotnoise[self.wgt_shotnoise < 0] = 0
+
+			self.invwgt_shotnoise[np.isnan(self.invwgt_shotnoise)] = 0
+			self.invwgt_shotnoise[np.isinf(self.invwgt_shotnoise)] = 0
+
+			# self.design[np.isnan(self.design)] = 0
+			# self.design[np.isinf(self.design)] = 0
+
+			# self.image[np.isnan(self.image)] = 0
+			# self.image[np.isinf(self.image)] = 0
+
+			## redo photometry
+
+			self.X_shotnoise = lstsq(
+				np.diag(np.sqrt(self.invwgt_shotnoise)) @ self.design,
+				self.image * np.sqrt(self.invwgt_shotnoise),
+			)[0]
+
+			self.flux_shotnoise = self.X_shotnoise[-1]
+
+			inv_cov = self.design.T @ np.diag(self.invwgt_shotnoise) @ self.design
+			try:
+				self.cov_shotnoise = np.linalg.inv(inv_cov)
+			except LinAlgError:
+				self.cov_shotnoise = np.linalg.pinv(inv_cov)
+			self.sigma_flux_shotnoise = np.sqrt(self.cov_shotnoise[-1, -1])
+
+		else:
+			self.flux_shotnoise = self.flux
+			self.sigma_flux_shotnoise = self.sigma_flux
+			self.X_shotnoise = self.X
+			self.cov_shotnoise = self.cov
+
+		self.pred_shotnoise = self.design @ self.X_shotnoise
+
+		self.mag_shotnoise = -2.5 * np.log10(self.flux_shotnoise) + 30
+		self.sigma_mag_shotnoise = (
+			2.5
+			* self.sigma_flux_shotnoise
+			/ np.sqrt((self.flux_shotnoise**2))
+			/ np.log(10)
+		)
+
+	def minimizeChisq(
+		self, x_init, size=30, background=False, method="Powell"
+	):
+		from scipy.optimize import minimize
+
+		self.solution = minimize(
+			chi2_multi,
+			x_init,
+			method=method,
+			args=(self, size, background),
+			options={"xtol": 0.01},
+		)
+		x_sol = self.solution.x
+		self.source_matrix = []
+		j = 0
+		for i in self.exposures[self.exposures["DETECTED"]]:
+			try:
+				self.source_matrix.append(construct_psf_source(
+						self.x_cen[i['EXPNUM']] + x_sol[j],
+						self.y_cen[i['EXPNUM']] + x_sol[j+1],
+						psf=self.psf[i["EXPNUM"]],
+						stampsize=size,
+						x_center=self.x_cen[i['EXPNUM']],
+						y_center=self.y_cen[i['EXPNUM']],
+						color = self.color[i['EXPNUM']]
+					))
+				j += 2 #this keeps track of indexing of x_sol
+				
+			except (OSError):
+					print(i['EXPNUM'])
+					self.source_matrix.append(np.zeros((size * size)))
+		self.constructDesignMatrix(size, background)
+		self.solvePhotometry(True, True)
+		self.shifts = x_sol
+		return x_sol
+
+	def minimizeChisqPerStamp(
+		self, x_init, size=30, stamp_inds = np.array([0, 10]), source_prior_mean = 0, source_prior_weight = 1, source_prior_inds = np.array([25, 50]), bg_prior_mean = 0, bg_prior_weight = 1, background=False, method="Powell"
+	):
+		from scipy.optimize import minimize
+		self.stamp_inds = stamp_inds
+		x_current = x_init
+		for i in range(self.stamp_inds[0], self.stamp_inds[1]):
+			print(f"Minimizing {self.exposures[self.exposures['DETECTED']][i]['EXPNUM']}")
+  
+			sol_this = minimize(
+			chi2_perstamp,
+			x_init[2*i:2*i+2],
+			method=method,
+			args=(self, i, size, source_prior_mean, source_prior_weight, source_prior_inds, bg_prior_mean, bg_prior_weight),
+			options={"xtol": 0.01},
+		)
+   
+			x_current[2*i] = sol_this.x[0]
+			x_current[2*i+1] = sol_this.x[1]
+  
+		#self.source_matrix = []
+		j = self.stamp_inds[0]
+		for i in self.exposures[self.stamp_inds[0]:self.stamp_inds[1]][self.exposures["DETECTED"][self.stamp_inds[0]:self.stamp_inds[1]]]:
+			try:
+				self.source_matrix[j] = construct_psf_source_z(
+						self.x_cen[i['EXPNUM']] + x_current[2*j],
+						self.y_cen[i['EXPNUM']] + x_current[2*j+1],
+						psf=self.psf[i["EXPNUM"]],
+						stampsize=size,
+						x_center=self.x_cen[i['EXPNUM']],
+						y_center=self.y_cen[i['EXPNUM']],
+						iz_color = self.color_iz[i['EXPNUM']]
+					)
+				j += 1 #this keeps track of indexing of x_sol
+				
+			except (OSError):
+					print(i['EXPNUM'])
+					self.source_matrix.append(np.zeros((size * size)))
+		self.shifts = x_current
+		self.constructDesignMatrix(size, background)
+		self.solvePhotometry(True, True, source_prior_mean, source_prior_weight, source_prior_inds, bg_prior_mean, bg_prior_weight)
+		return x_current
+
+
+def chi2_multi(x, detection, size = 30, background = False):
+	j = 0	
+	detection.source_matrix = []
+	for i in detection.exposures[detection.exposures['DETECTED']]: 
+		detection.source_matrix.append(construct_psf_source(detection.x_cen[i['EXPNUM']] + x[j], detection.y_cen[i['EXPNUM']] + x[j+1], detection.psf[i['EXPNUM']], size, detection.x_cen[i['EXPNUM']], detection.y_cen[i['EXPNUM']]))
+		j += 2
+	detection.constructDesignMatrix(size, background)
+	detection.solvePhotometry(True, True)
+	chisq = np.sum(detection.res * detection.res * detection.invwgt)
+
+	return chisq 
+
+def chi2_perstamp(x, detection, stamp, size = 30, source_prior_mean = 0, source_prior_weight = 1, source_prior_inds = np.array([25, 50]), bg_prior_mean = 0, bg_prior_weight = 1):
+	expnum = detection.exposures[detection.exposures['DETECTED']][stamp]['EXPNUM']
+	detection.source_matrix[stamp] = construct_psf_source_z(detection.x_cen[expnum] + x[0], detection.y_cen[expnum] + x[1], detection.psf[expnum], size, detection.x_cen[expnum], detection.y_cen[expnum], detection.color_iz[expnum])
+	detection.updateDesignMatrix(size,stamp, source_prior_mean, source_prior_weight, source_prior_inds, bg_prior_mean, bg_prior_weight)
+	prod = detection.diag.dot(detection.design)
+	detection.lsqr = sp.linalg.lsqr(prod, detection.target, x0 = detection.lsqr[0])
+
+	return detection.lsqr[3] 
